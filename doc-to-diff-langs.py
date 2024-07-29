8d39fc0f2b89e752d0616e3864c0240e2c358942
Daniel Tamakloe <37350796+301202@users.noreply.github.com>
2024-07-29 17:08:31 +0000

    so program only works with .txt files for now

--- doc-to-diff-langs.py
+++ doc-to-diff-langs.py
@@ -1,22 +1,22 @@
 import googletrans
 from googletrans import Translator
-file = "Letter of Intent.docx"
+file = "testfile.txt"
 
 translatedtext = 'de'
 filetranslator = Translator()
 detectlang = filetranslator.detect(file).lang.lower()
 destlang = translatedtext.lower()
 
-with open(file, 'r', encoding='utf-8') as file:
+with open(file, 'r') as file:
     #file.write("Hello, this is a test file.")
     test = file.read()
 
-newfile = "Letter of Intent" + destlang + " .docx"
+newfile = "testfile" + destlang + ".txt"
 
 translation = filetranslator.translate(test, src=detectlang, dest=destlang)
 with open(newfile, 'w') as f:
     f.write(translation.text)
 
-print(test)
+#print(test)
 #print(f"file '{file}' was created.")
 print(f"file '{newfile}' was created.")
